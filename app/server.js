a1df65e4c20ec623f0a848b808e5bb3bf6eee3ec
Willian Johan <johan_willian@hotmail.com>
2022-03-24 01:59:52 -0300

    index database atualizado

--- app/server.js
+++ app/server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 //const config = require('config') || 3000;
-
+require('./database')
 
 app.use(bodyParser.json());
 
-const routes = require('./routes/routes');
+const routes = require('./routes/routes')
 app.use(routes)
 
 
 app.listen(3000, () => {
-    console.log('API initialized.....');
-    console.log('API listening on port: %d.....',3000);
+   console.log('API initialized.....')
+   console.log('API listening on port: %d.....',3000)
 })
