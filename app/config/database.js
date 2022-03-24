2f1eb50a46b6f0fc0d70ad18784765580586b7e2
Willian Johan <johan_willian@hotmail.com>
2022-03-24 01:51:33 -0300

    informacoes de configuracao do database atualizados

--- app/config/database.js
+++ app/config/database.js
@@ -1,11 +1,14 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'postgres://spqmrkzo:s8GG7hnD6jr8lGGngKk4yjybR2fiIwXM@tuffi.db.elephantsql.com/spqmrkzo',
+    host: 'tuffi.db.elephantsql.com',
     username: 'spqmrkzo',
     password: 's8GG7hnD6jr8lGGngKk4yjybR2fiIwXM',
-    database: 'fluxodecaixa_db',
+    database: 'spqmrkzo',
     define: {
         timestamps: true, //Armazena informação de data de criação e alteração
         underscored: true, //Permite formato snake case (exemplo: grupo_usuarios)
     }
 }
+
+//postgres://spqmrkzo:s8GG7hnD6jr8lGGngKk4yjybR2fiIwXM@tuffi.db.elephantsql.com/spqmrkzo
+//postgres://username:password@hostname/databasename
