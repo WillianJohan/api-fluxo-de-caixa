79c285afe9040c2d1e25304bbb6e1ec773835044
Willian Johan <johan_willian@hotmail.com>
2022-03-24 01:58:35 -0300

    rota de users atualizado

--- app/routes/modules/users.js
+++ app/routes/modules/users.js
@@ -1,9 +1,9 @@
 const userRouter = require('express').Router();
+const UserController = require('../../controllers/UserController')
 
 //Requer autorização => lista usuarios
-userRouter.get('/', (req, res, next) => {
-    res.json( { Message:"users! => GET" } );
-})
+userRouter.get('/', UserController.index)
+userRouter.post('/create', UserController.store)
 
 
 module.exports = userRouter;
