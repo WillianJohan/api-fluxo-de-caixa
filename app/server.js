e957f1d0706e1737ccd571c4b57236fa5148bea7
Willian Johan <johan_willian@hotmail.com>
2022-03-23 21:48:26 -0300

    app/index.js renomeado para server.js

---
+++ app/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+//const config = require('config') || 3000;
+
+
+app.use(bodyParser.json());
+
+const routes = require('./routes/routes');
+app.use(routes)
+
+
+app.listen(3000, () => {
+    console.log('API initialized.....');
+    console.log('API listening on port: %d.....',3000);
+})
