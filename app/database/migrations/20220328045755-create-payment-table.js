d77776af642b49992d8eed8384d8452de669bfd9
Willian Johan <johan_willian@hotmail.com>
2022-03-28 02:38:44 -0300

    Criado migrate payment do banco

---
+++ app/database/migrations/20220328045755-create-payment-table.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+     await queryInterface.createTable('payments', { 
+       id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        value: {
+          type: Sequelize.DOUBLE,
+          allowNull: false,
+        },
+        date: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        is_done: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        }
+      });
+  },
+
+  async down (queryInterface, Sequelize) {
+     await queryInterface.dropTable('payments');
+  }
+};
