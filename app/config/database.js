620c51781bc708471c32ed501fd9e1c28da21351
Willian Johan <johan_willian@hotmail.com>
2022-03-27 21:57:11 -0300

    Senha do banco ofuscada

--- app/config/database.js
+++ app/config/database.js
@@ -2,13 +2,10 @@ module.exports = {
     dialect: 'postgres',
     host: 'tuffi.db.elephantsql.com',
     username: 'spqmrkzo',
-    password: 's8GG7hnD6jr8lGGngKk4yjybR2fiIwXM',
+    password: process.env.DATABASE_PASSWORD,
     database: 'spqmrkzo',
     define: {
         timestamps: true, //Armazena informação de data de criação e alteração
         underscored: true, //Permite formato snake case (exemplo: grupo_usuarios)
     }
 }
-
-//postgres://spqmrkzo:s8GG7hnD6jr8lGGngKk4yjybR2fiIwXM@tuffi.db.elephantsql.com/spqmrkzo
-//postgres://username:password@hostname/databasename
