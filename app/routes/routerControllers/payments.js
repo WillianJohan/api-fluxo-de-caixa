4755c0f21050ff2e3323034f537e642d33eab302
Willian Johan <johan_willian@hotmail.com>
2022-03-28 02:38:56 -0300

    rotas do payment definidas

--- app/routes/routerControllers/payments.js
+++ app/routes/routerControllers/payments.js
@@ -1,9 +1,14 @@
 const paymentsRouter = require('express').Router();
+const PaymentController = require('../../controllers/PaymentController');
+
+//Importa os middlewares que verifica as permissões
+const { client, admin } = require('../../middleware/Authenticate')
 
 //Requer autorização => lista de pagamentos
-paymentsRouter.get('/', (req, res) => {
-    res.json( { Message:"payments! => GET" } );
-})
+paymentsRouter.get('/', client, PaymentController.index)
+paymentsRouter.post('/create', admin, PaymentController.store)
+paymentsRouter.patch('/update/:id', admin, PaymentController.patch)
+paymentsRouter.post('/remove', admin, PaymentController.remove)
 
 
 module.exports = paymentsRouter;
